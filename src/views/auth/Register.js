a3eeec74670af99156e7275d36ffc1d8cb18da5c
anas123x <anas.dai@esprit.tn>
2024-02-25 20:25:05 +0100

    candiat+staff sign up design

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,6 +1,18 @@
-import React from "react";
+import{ React,useState} from "react";
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 
 export default function Register() {
+  const [country, setCountry] = useState('');
+  const [region, setRegion] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const selectCountry = (value) => {
+    setCountry(value);
+  };
+
+  const selectRegion = (value) => {
+    setRegion(value);
+  };
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -75,26 +87,97 @@ export default function Register() {
                   </div>
 
                   <div className="relative w-full mb-3">
-                  <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    <div className="flex justify-between w-[80%]">
+                  <div className="flex flex-col">
+                  <div className="relative">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlFor="grid-password"
+                        >
+                          <i className="fa-solid fa-key"></i>
+                          {'  '} Password
+                        </label>
+                        <div className="relative flex items-center">
+                        <input
+                          type={passwordVisible ? "text" : "password"}
+                          className="border-0 px-3 py-3 mr-3 rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                        <i 
+                          onClick={() => setPasswordVisible(!passwordVisible)}
+                          className={`fa-solid ${passwordVisible ? 'fa-eye-slash' : 'fa-eye'} absolute mr-3 right-3 top-1/2 transform -translate-y-1/2 cursor-pointer `}
+                        />
+                        </div>
+                      </div>
+                                          </div>
+                    <div className="flex flex-col">
+                     <label
+                      className=" uppercase text-blueGray-600 text-xs font-bold mb-2"
                       htmlFor="grid-password"
                     >
-                      <i class="fa-solid fa-key"></i>
-                        {'  '} Password
+                      <i class="fa-solid fa-lock"></i>
+                        {'  '}Confirm Password
                     </label>
-                    <input
+                      <input
                       type="password"
-                      className="border-0 px-3 py-3   rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red  focus:ring w-full ease-linear transition-all duration-150 "
+                      className="border-0 px-3 py-3  rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red  focus:ring w-full ease-linear transition-all duration-150 "
                     
                     />
+                    </div>
+                    </div>
                   </div>
+                    <div className="relative  mt-4 mb-3">
 
+                      <label
+                        className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        htmlFor="grid-role"
+                      >
+                                            <i class="fa-solid fa-graduation-cap"></i>
+
+                                            {'  '}Role
+                     
+                      </label>
+                      <select
+                        id="grid-role"
+                        className="border-0 px-3 py-3 mr-3 rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red focus:ring w-full ease-linear transition-all duration-150"
+                      >
+                        <option value="student">Student</option>
+                        <option value="alumni">Alumni</option>
+                        <option value="staff">Staff</option>
+                      </select>
+                    </div>
+                  <div>
+                  <label
+                      className=" uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      <i class="fa-solid fa-earth-africa"></i>
+                        {'  '}Select your country
+                    </label>
+                    <CountryDropdown
+                      value={country}
+                      onChange={(val) => selectCountry(val)} 
+                      className="border-0 mb-3 mt-2 px-3 py-3 mr-3  rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red  focus:ring w-full ease-linear transition-all duration-150 "
+                      />
+                         <label
+                      className=" uppercase text-blueGray-600 text-xs font-bold mb-4"
+                      htmlFor="grid-password"
+                    >
+                      <i class="fa-solid fa-mountain-sun"></i>
+                        {'  '}Select your region
+                    </label>
+                    <RegionDropdown
+                      country={country}
+                      value={region}
+                      onChange={(val) => selectRegion(val)}
+                      className="border-0 mb-3 mt-2 px-3 py-3 mr-3  rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red  focus:ring w-full ease-linear transition-all duration-150 "
+                      />
+                  </div>
                   <div>
                     <label className="inline-flex items-center cursor-pointer">
                       <input
                         id="customCheckLogin"
                         type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear focus:ring-custom-red transition-all duration-150"
                       />
                       <span className="ml-2 text-sm font-semibold text-blueGray-600">
                         I agree with the{" "}
