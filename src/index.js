69a6b51e847ad586b155c5c04973b00c6bf20ddd
anas123x <anas.dai@esprit.tn>
2024-03-06 11:13:41 +0100

    zlast

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route,Routes,Navigate} from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import ContactUs from 'views/ContactUs';
-import AdminContactPage from 'views/admin/AdminContactPage';
+
 import "assets/styles/index.css";
 
 // layouts
@@ -14,7 +13,8 @@ import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import ContactUs from "views/ContactUs.js";
+import AdminContactPage from "views/admin/AdminContactPage.js";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="305919606485-hj7u2mmjvcoaa7blqet64uglmpu3e6aa.apps.googleusercontent.com">
 
@@ -27,15 +27,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/landing" exact element={<Landing />} />
       <Route path="/profile" exact element={<Profile/>} />
       <Route path="/" exact element={<Index />}/>
-      {/* add redirect for first page */}
-      <Route path="*" element={<Navigate to="/" />} />
-      <Route path='/contactus' exact element={<ContactUs />} />
+          <Route path='/contactus' exact element={<ContactUs />} />
       <Route path='/contactAdmin' exact element={<AdminContactPage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+
     </Routes>
   </BrowserRouter>
       </GoogleOAuthProvider>,
 
  
 );
-
-ReactDOM.createRoot(document.getElementById('root')).render(<MainApp />);
