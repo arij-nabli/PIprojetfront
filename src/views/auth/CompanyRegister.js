a057885f0cac22739d23cab9bf8084506c4a7a30
anas123x <anas.dai@esprit.tn>
2024-03-14 00:36:26 +0100

    company sign up country

--- src/views/auth/CompanyRegister.js
+++ src/views/auth/CompanyRegister.js
@@ -2,6 +2,7 @@ import { React, useState } from "react";
 import axios from "axios";
 import IntlTelInput from "react-intl-tel-input";
 import "react-intl-tel-input/dist/main.css";
+import ReactCountryFlagsSelect from "react-country-flags-select";
 
 export default function CompanyRegister() {
   const [redirectToSignIn, setRedirectToSignIn] = useState(false);
@@ -9,6 +10,9 @@ export default function CompanyRegister() {
   const [errorMessage, setErrorMessage] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [logo, setLogo] = useState(null);
+  const [country, setCountry] = useState("");
+  const [selected, setSelected] = useState("");
+
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
@@ -26,7 +30,17 @@ export default function CompanyRegister() {
   const [showModal, setShowModal] = useState(false);
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
+  const selectCountry = (value) => {
+    console.log(value);
+    setSelected(value);
+    setCountry(value);
+  };
   const signUp = () => {
+    if (!isFormValid()) {
+      setErrorMessage('Please fill in all fields');
+      return;
+    }
+   
     const companyData = {
       username: nomEntreprise,
       email: email,
@@ -36,37 +50,24 @@ export default function CompanyRegister() {
       industry: secteurActivite,
       description: descreption,
       phone: telephone,
-      country:{label:"tunisia"},
       location: adresse,
     };
     console.log("Company Data:", companyData);
-    axios
-      .post(
-        "http://localhost:5000/auth/register",
-        JSON.stringify(companyData),
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
+    axios.post('http://localhost:5000/auth/register', JSON.stringify(companyData), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
         console.log(response.data);
-        setErrorMessage(
-          "Company registered successfully! Please check your email to verify your account."
-        );
+        setErrorMessage("Your Request will be validated by an admin.S")
       })
-      .catch((error) => {
-        if (
-          error.response &&
-          error.response.data &&
-          error.response.data.message
-        ) {
-          setErrorMessage(error.response.data.message);
-        }
+      .catch(error => {
+        console.log(error)
+        if(error.response.data.message === "User already exists")
+        setErrorMessage("This company has already been registered")
       });
   };
-
   const handleConditionsChange = () => {
     setAcceptedConditions(!acceptedConditions);
   };
@@ -99,18 +100,24 @@ export default function CompanyRegister() {
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-
+  
     const errors = [];
-    if (event.target.value.length < 6) {
-      errors.push("Password must contain at least 6 characters");
+    if (event.target.value.length < 8) {
+      errors.push('Password must contain at least 8 characters');
     }
     if (!/[A-Z]/.test(event.target.value)) {
-      errors.push("Password must contain an uppercase letter");
+      errors.push('Password must contain an uppercase letter');
+    }
+    if (!/[a-z]/.test(event.target.value)) {
+      errors.push('Password must contain a lowercase letter');
     }
     if (!/\d/.test(event.target.value)) {
-      errors.push("Password must contain a number");
+      errors.push('Password must contain a number');
     }
-
+    if (!/[!@#$%^&*]/.test(event.target.value)) {
+      errors.push('Password must contain a special character');
+    }
+  
     setPasswordError(errors);
   };
 
@@ -129,10 +136,10 @@ export default function CompanyRegister() {
 
   const handleTelephoneChange = (e) => {
     const value = e.target.value;
-      console.log(value)
+
     // Add any validation logic here
     if (value.trim() === "") {
-      setTelephoneError("Téléphone ne peut pas être vide");
+      setTelephoneError("phone in valid ");
     } else {
       setTelephoneError("");
     }
@@ -148,7 +155,7 @@ export default function CompanyRegister() {
     // Validation supplémentaire si nécessaire
     if (!/^\d+$/.test(numericValue)) {
       setTelephoneError(
-        "Le numéro de téléphone doit contenir uniquement des chiffres."
+        "The telephone number must contain only number."
       );
     } else {
       setTelephoneError("");
@@ -167,9 +174,11 @@ export default function CompanyRegister() {
       secteurActivite &&
       descreption &&
       adresse &&
-      telephone
+      telephone&&
+      website 
     );
   };
+ 
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -275,6 +284,25 @@ export default function CompanyRegister() {
                       )}
                     </div>
                   </div>
+                  <div className="flex justify-between mb-3">
+                      <div className="w-1/2 mr-3">
+                        <label
+                          className=" uppercase text-blueGray-600 text-xs  font-bold mb-3"
+                          htmlFor="grid-password"
+                        >
+                          <i class="fa-solid fa-earth-africa"></i>
+                          {"  "}Select your country
+                        </label>
+                        <ReactCountryFlagsSelect
+                          selected={selected}
+                          onSelect={(e) => selectCountry(e)}
+                          searchable={true}
+                          selectHeight={45}
+                        />
+                        
+                      </div>
+                    </div>
+                   
                   <div className="flex flex-col lg:flex-row">
                     {/* Phone on the Left */}
 
@@ -290,17 +318,16 @@ export default function CompanyRegister() {
                         <div className="flex items-center">
                           <IntlTelInput
                             containerClassName="intl-tel-input"
+                            inputClassName="border-0 bg-gray-100 bg-gray-100 px-3 py-3 rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red focus:ring w-full ease-linear transition-all duration-150"
                             
-                            inputComponent={({ value, onChange }) => (
-                              <input
+                             
                                 type="tel"
-                                className="border-0 bg-gray-100 px-3 py-3 rounded text-sm shadow focus:outline-none focus:border-0 focus:ring-custom-red focus:ring w-full ease-linear transition-all duration-150"
-
+                               
                                 value={telephone}
                                 onChange={handleTelephoneChange}
-                              />
+                            
 
-                            )}
+                          
                             preferredCountries={[]}
                             defaultCountry="tn"
                             format
@@ -396,37 +423,16 @@ export default function CompanyRegister() {
                             } absolute mr-3 right-3 top-1/2 transform -translate-y-1/2 cursor-pointer`}
                           />
                         </div>
-                        {passwordError.length > 0 && (
-                          <div className="text-red-500 text-base mt-2 space-y-2">
-                            {passwordError.map((error, index) => (
-                              <div key={index} className="flex items-center">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                  className="h-5 w-5 mr-2"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M6 18L18 6M6 6l12 12"
-                                  />
-                                </svg>
-                                <p>{error}</p>
-                              </div>
-                            ))}
-                          </div>
-                        )}
+                        
 
-                        {passwordError.length > 0 && (
-                          <div className="text-red-500 text-xs mt-2">
-                            {passwordError.map((error, index) => (
-                              <p key={index}>{error}</p>
-                            ))}
-                          </div>
-                        )}
+{passwordError.length > 0 && (
+  <div className="text-red-500 text-xs mt-2">
+    {passwordError.map((error, index) => (
+      <p key={index}>{error}</p>
+    ))}
+  </div>
+)}
+
                       </div>
                     </div>
 
@@ -490,6 +496,7 @@ export default function CompanyRegister() {
                   </div>
 
                   <div className="text-center mt-6">
+                 
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="button"
@@ -498,28 +505,15 @@ export default function CompanyRegister() {
                       Create Account
                     </button>
                   </div>
-                  {errorMessage &&
-                  errorMessage !=
-                    "Your Request will be validated by an admin as soon as possible !Please check your email " ? (
-                    <div
-                      class="p-4 mb-4 flex justify-center relative text-sm text-red-800 rounded-lg bg-red-200 mt-5  dark:text-red-400"
-                      role="alert"
-                    >
-                      <span class="font-medium">
-                        {" "}
-                        <p style={{ color: "red" }}>{errorMessage}</p>
-                      </span>
-                    </div>
-                  ) : null}
-                  {errorMessage ==
-                  "User registered successfully! Please check your email to verify your account." ? (
-                    <div
-                      class="p-4 mt-4 mb-4 text-sm text-center text-green-800 rounded-lg bg-green-50 bg-green-200 dark:text-green-700"
-                      role="alert"
-                    >
-                      <span class="font-medium ">{errorMessage}</span>
-                    </div>
-                  ) : null}
+                 
+                  {errorMessage && errorMessage != "Your Request will be validated by an admin.S"?<div class="p-4 mb-4 flex justify-center relative text-sm text-red-800 rounded-lg bg-red-200 mt-5  dark:text-red-400" role="alert">
+  <span class="font-medium"> <p style={{  color: 'red' }}>{errorMessage}</p></span>
+</div>            : null   }
+                      {errorMessage == "Your Request will be validated by an admin.S" ?
+                       <div class="p-4 mt-4 mb-4 text-sm text-center text-green-800 rounded-lg bg-green-50 dark:text-green-700" role="alert">
+                       <span class="font-medium ">{errorMessage}</span> 
+                     </div> :
+                        null}
                 </form>
               </div>
             </div>
