dc1309905c942832caf886f0ba0ad485cee20b1f
anas123x <anas.dai@esprit.tn>
2024-03-13 18:18:59 +0100

    Fix logout button styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -184,7 +184,7 @@ export default function Sidebar() {
                   Industries
                 </Link>
               </li>
-              <button className="items-center mb-1 bg-red-500 flex justify-center w-full bottom-0 absolute text-xs uppercase py-3 font-bold  text-black">
+              <button className="items-center mb-1 bg-red-500 flex justify-center w-full bottom-0 absolute text-xs uppercase py-3 font-bold  text-black" onClick={logout}>
                
                   <i class="fa-solid fa-arrow-right-from-bracket  mr-2 text-sm  opacity-75 text-blueGray-300" style={{color:"black"}}></i>
                  {" "}
