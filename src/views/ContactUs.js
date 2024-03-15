c381cc4aa6c0cacf0ed434e889aaae807c747427
anas123x <anas.dai@esprit.tn>
2024-03-15 23:47:52 +0100

    remove .css import

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import '../assets/styles/contact.css'
 
 // components
 import Navbar from 'components/Navbars/AuthNavbar'
